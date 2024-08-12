f7e38d4d28903bcde88f7dba3a2cfbe0d4a744ed
taojingzhi <taojingzhi2020@163.com>
2024-08-12 23:58:22 +0800

    测试提交

--- mall-tiny-01/src/main/java/com/macro/mall/tiny/MallTinyApplication.java
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/MallTinyApplication.java
@@ -2,12 +2,18 @@ package com.macro.mall.tiny;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MallTinyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MallTinyApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(MallTinyApplication.class, args);
+//        String[] beanDefinitionNames = run.getBeanDefinitionNames();
+//        for (String b:beanDefinitionNames
+//             ) {
+//            System.out.println(b);
+//        }
     }
 
 }
